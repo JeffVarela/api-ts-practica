76f92dba64badd60f73bac6e8b2c01605b06a502
Jeff <jeffersonvarela.jv@hotmail.com>
2021-10-03 21:34:41 -0600

    .

---
+++ functions/lib/routes/products.routes.js
@@ -0,0 +1,70 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const Router = require("express");
+const admin = require("firebase-admin"); /* requerimos de firebase admin */
+const router = Router();
+/* registramos nuestra api con los datos de la bd de firebase */
+const db = admin.firestore();
+router.post('/api/products', async (req, res) => {
+    try {
+        await db.collection('productos')
+            .doc('/' + req.body.id + '/') /* aqui creamos el id, si lo dejamos vacío firebase creara el id automaticamente */
+            .create({ name: req.body.name });
+        return res.status(204).json();
+    }
+    catch (error) {
+        console.log(error);
+        return res.status(500).send(error);
+    }
+});
+router.get('/api/products/:products_id', async (req, res) => {
+    try {
+        const doc = db.collection('productos').doc(req.params.products_id);
+        const item = await doc.get();
+        const response = item.data();
+        return res.status(200).json(response);
+    }
+    catch (error) {
+        return res.status(500).send(error);
+    }
+});
+router.get('/api/products', async (req, res) => {
+    try {
+        const query = db.collection('productos');
+        const querySnapshot = await query.get(); /* nos devuelve un arreglo con todos loas datos */
+        const docs = querySnapshot.docs;
+        /* pero debemos interpretarlo */
+        const response = docs.map((doc) => ({
+            id: doc.id,
+            name: doc.data().name,
+        }));
+        return res.status(200).json(response);
+    }
+    catch (error) {
+        return res.status(500).json();
+    }
+});
+router.delete('/api/products/:products_id', async (req, res) => {
+    try {
+        const documents = db.collection('productos').doc(req.params.products_id);
+        await documents.delete();
+        return res.status(200).json();
+    }
+    catch (error) {
+        return res.status(500).json();
+    }
+});
+router.put('/api/products/:products_id', async (req, res) => {
+    try {
+        const documents = db.collection('productos').doc(req.params.products_id);
+        await documents.update({
+            name: req.body.name
+        });
+        return res.status(200).json();
+    }
+    catch (error) {
+        return res.status(500).json();
+    }
+});
+module.exports = router;
+//# sourceMappingURL=products.routes.js.map
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,94 +3,24 @@ import * as admin from 'firebase-admin' /* requerimos de firebase admin */
 import * as express from "express"
 
 const app = express()
-/* registramos nuestra api con los datos de la bd de firebase */
+
 admin.initializeApp({
   credential: admin.credential.cert('./credential.json'),
   databaseURL: 'https://fb-api-da617.firebaseio.com'
 })
 
-const db = admin.firestore()
-
-/*  */
 app.get('/hello-world', (req, res) => {
-  return res.status(200).json({message: 'hello world'})
+  return res.status(200).json({ message: 'hello world' })
 })
 
-app.post('/api/products', async (req, res) => {
- try {
-  await db.collection('productos')
-  .doc('/' + req.body.id + '/') /* aqui creamos el id, si lo dejamos vacío firebase creara el id automaticamente */
-  .create({ name: req.body.name })
-  return res.status(204).json();
- }
-  catch (error) {
-   console.log(error);
-   return res.status(500).send(error)
- }
-})
-
-app.get('/api/products/:products_id', async (req, res) => {
-  
-    try {
-      const doc = db.collection('productos').doc(req.params.products_id)
-      const item = await doc.get();
-      const response = item.data();
-      return res.status(200).json(response);
-    }
-     catch (error) {
-     return res.status(500).send(error)   
-    }
-  
-})
-
-app.get ('/api/products', async (req, res) =>{
-  try {
-    
-   const query = db.collection('productos');
-   const querySnapshot = await query.get(); /* nos devuelve un arreglo con todos loas datos */
-   const docs = querySnapshot.docs;
-
-   /* pero debemos interpretarlo */
-   const response = docs.map(  (doc) => ({
-     id: doc.id,
-     name: doc.data().name,
-   }));
-   return res.status(200).json(response)
-
-  } catch (error) {
-    return res.status(500).json();
-  }
-})
-
-app.delete('/api/products/:products_id', async (req, res)=>{
- try {
-  const documents = db.collection('productos').doc(req.params.products_id);
-  await documents.delete();
-  return res.status(200).json();
-
- } catch (error) {
-  return res.status(500).json(); 
- }
-})
-
-app.put('/api/products/:products_id', async (req, res)=>{
-  try {
-    const documents = db.collection('productos').doc(req.params.products_id);
-  await documents.update({
-    name: req.body.name
-  });
-  return res.status(200).json();
-  } catch (error) {
-  return res.status(500).json(); 
-  }
-})
+app.use(require('./routes/products.routes'))
 
 exports.app = functions.https.onRequest(app)
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 
- /* export const helloWorld = functions.https.onRequest((request, response) => {
-   functions.logger.info("Hello logs!", {structuredData: true});
-   response.send("Hello from Firebase!");
+/* export const helloWorld = functions.https.onRequest((request, response) => {
+  functions.logger.info("Hello logs!", {structuredData: true});
+  response.send("Hello from Firebase!");
 }); */
